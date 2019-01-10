fedbc69e50901e3e37a5a0e1bb43df42e5ff5e0a
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2019-01-10 19:14:45 +0530

    Handling maximum all chances of runs and outs.
    Not handling batsmen and bowler names.

---
+++ controller.js
@@ -0,0 +1,24 @@
+const getId = function(Id) {
+  return document.getElementById(Id);
+};
+
+const displayPlayers = function({ batsmen, bowler, score }) {
+  getId('1').innerText = batsmen.player1.name;
+  getId('2').innerText = batsmen.player2.name;
+  getId('3').innerText = bowler.name;
+  getId('4').innerText = score.overs;
+  getId('5').innerText = score.total;
+  getId('6').innerText = score.wickets;
+};
+
+let scoreCard = new createInitialScoreCard('sachin', 'dhoni', 'zaheer');
+
+const update = function() {
+  let ballStatus = getId('input').value;
+  scoreCard.updateScore(ballStatus);
+  displayPlayers(scoreCard);
+};
+
+window.onload = function() {
+  displayPlayers(scoreCard);
+};
---
+++ model.js
@@ -0,0 +1,78 @@
+const createPlayer = function(playerName, status) {
+  let name = playerName;
+  let runs = '';
+  let balls = '';
+  return { name, status, runs, balls };
+};
+
+const createBatsmen = function(playerName1, playerName2) {
+  let player1 = createPlayer(playerName1, 'stike');
+  let player2 = createPlayer(playerName2, 'nonStrike');
+  return { player1, player2 };
+};
+
+const createBowler = function(playerName) {
+  let name = playerName;
+  let balls = '';
+  return { name, balls };
+};
+
+const createScore = function() {
+  let total = 0;
+  let overs = 0;
+  let wickets = 0;
+  return { total, overs, wickets };
+};
+
+const hasRuns = function(ballStatus) {
+  let runs = ballStatus.split('+');
+  runs = runs[runs.length - 1];
+  const validRuns = ['0', '1', '2', '3', '4', '6'];
+  if (validRuns.includes(runs)) {
+    return +runs;
+  }
+  return 0;
+};
+
+const isOut = function(ballStatus) {
+  const chances = ['caught', 'wicket', 'bowled', 'runout', 'lbw'];
+  if (chances.includes(ballStatus)) {
+    return 1;
+  }
+  return 0;
+};
+
+const validateScore = function(ballStatus) {
+  let runs = hasRuns(ballStatus);
+  let balls = 0.1;
+  let wickets = isOut(ballStatus);
+  if (ballStatus.includes('wide') || ballStatus.includes('noball')) {
+    runs += 1;
+    return { runs, balls: 0, wickets };
+  }
+  return { runs, balls, wickets };
+};
+
+const convertBallsToOvers = function(overs) {
+  let decimalValue = overs - Math.floor(overs / 1);
+  if (decimalValue > 0.5) {
+    return overs + 1 - decimalValue;
+  }
+  return overs;
+};
+
+class createInitialScoreCard {
+  constructor(player1, player2, bowler) {
+    this.batsmen = createBatsmen(player1, player2);
+    this.bowler = createBowler(bowler);
+    this.score = createScore();
+  }
+  updateScore(ballStatus) {
+    let { runs, balls, wickets } = validateScore(ballStatus);
+    this.score.total += runs;
+    this.score.overs += balls;
+    this.score.overs = +convertBallsToOvers(this.score.overs).toFixed(1);
+    this.score.wickets += wickets;
+    return this;
+  }
+}
